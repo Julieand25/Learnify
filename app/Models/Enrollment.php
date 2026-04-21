fc75e498724d122a261c649952ee6f6e1d65a124
julieand25 <julieandmojunggal@gmail.com>
2026-04-21 11:37:35 +0800

    fix quiz progress

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -10,22 +10,31 @@ class Enrollment extends Model
 
     protected static function booted(): void
     {
-        static::deleted(function (Enrollment $enrollment) {
-            NotepadNote::where('student_id', $enrollment->student_id)
-                ->where('class_room_id', $enrollment->class_room_id)
-                ->delete();
-
-            ChapterProgress::where('student_id', $enrollment->student_id)
-                ->where('class_room_id', $enrollment->class_room_id)
-                ->delete();
-
-            $quizIds = Quiz::where('class_room_id', $enrollment->class_room_id)
-                ->pluck('id');
-
-            QuizAttempt::where('student_id', $enrollment->student_id)
-                ->whereIn('quiz_id', $quizIds)
-                ->delete();
+        static::created(function (Enrollment $enrollment) {
+            self::clearClassData($enrollment);
         });
+
+        static::deleted(function (Enrollment $enrollment) {
+            self::clearClassData($enrollment);
+        });
+    }
+
+    private static function clearClassData(Enrollment $enrollment): void
+    {
+        NotepadNote::where('student_id', $enrollment->student_id)
+            ->where('class_room_id', $enrollment->class_room_id)
+            ->delete();
+
+        ChapterProgress::where('student_id', $enrollment->student_id)
+            ->where('class_room_id', $enrollment->class_room_id)
+            ->delete();
+
+        $quizIds = Quiz::where('class_room_id', $enrollment->class_room_id)
+            ->pluck('id');
+
+        QuizAttempt::where('student_id', $enrollment->student_id)
+            ->whereIn('quiz_id', $quizIds)
+            ->delete();
     }
 
     public function student()
